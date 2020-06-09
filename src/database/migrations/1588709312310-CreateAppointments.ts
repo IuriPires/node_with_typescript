961bde40cec3ea23b70f99d746b3671f34743d36
Iuri Pires <iuripires.work@gmail.com>
2020-06-09 12:20:06 -0300

    AlterProviderFieldToProviderId migration created and Appointment model altered with relation between user and appointment

--- src/database/migrations/1588709312310-CreateAppointments.ts
+++ src/database/migrations/1588709312310-CreateAppointments.ts
@@ -9,7 +9,7 @@ export class CreateAppointments1588709312310 implements MigrationInterface {
           columns: [
             {
               name: 'id',
-              type: 'varchar',
+              type: 'uuid',
               isPrimary: true,
               generationStrategy: 'uuid',
               default: 'uuid_generate_v4()'
