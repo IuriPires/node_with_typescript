83659d1825e806fd7dd30cf396681dcec2e5bb34
Iuri Pires <iuripires.work@gmail.com>
2020-08-03 12:35:12 -0300

    still working on it

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -5,7 +5,7 @@ import AuthenticateUserService from '../service/AuthenticateUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-  try {
+
     const {email, password} = request.body;
 
     const authenticateUser = new AuthenticateUserService();
@@ -18,10 +18,6 @@ sessionsRouter.post('/', async (request, response) => {
     delete user.password;
 
     return response.json({ user, token });
-
-  } catch(err) {
-    return response.status(err.statusCode).json({ error: err.message });
-  }
 });
 
 export default sessionsRouter;
