30224ac2db3cfaafd81db4db66f19bc76bcef5cf
Iuri Pires <iuripires.work@gmail.com>
2020-08-22 14:39:40 -0300

    added

--- src/modules/appoitments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appoitments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,8 +1,10 @@
+import IAppointmentsRepository, { IAppointmentsRepository } from '@modules/appoitments/repositories/IAppointmentsRepository';
+
 import Appointment from '../infra/typeorm/entities/Appointment';
 import { EntityRepository, Repository } from 'typeorm';
 
 @EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment> {
+class AppointmentsRepository extends Repository<Appointment> implements IAppointmentsRepository{
   public async findByDate(date: Date): Promise<Appointment | null> {
     const findAppointment = await this.findOne({
       where: { date }
